a0c03931cf167ebbb3873bc4d3034ac29c0912b9
POlesya <olesya.pilip@gmail.com>
2023-10-05 22:17:06 +0300

    Task 5

--- Homework_5/main_1.java
+++ Homework_5/main_1.java
@@ -2,23 +2,46 @@ package Homework_5;
 
 public class main_1 {
     public static void main(String[] args) {
-        int n = 37; // Number of values sin(x)
-        double[] sinValues = new double[n];
+        int[][] matrix = {
+                {1, -3, 8},
+                {3, -2, 6},
+                {5, 6, -9}
+        };
 
-        for (int i = 0; i < n; i++) {
-            int x = i * 10;
-            double sinX = Math.sin(Math.toRadians(x));
-            sinValues[i] = sinX;
+        int negativeCount = countNegativeBelowDiagonal(matrix);
+        int oddSum = sumOddAboveDiagonal(matrix);
+
+        System.out.println("The number of negative elements below the main diagonal: " + negativeCount);
+        System.out.println("The sum of odd elements above the main diagonal: " + oddSum);
+    }
+
+    public static int countNegativeBelowDiagonal(int[][] matrix) {
+        int count = 0;
+        int n = matrix.length;
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (matrix[i][j] < 0) {
+                    count++;
+                }
+            }
         }
 
-        // Let's display the table of values sin(x)
-        System.out.println("x\t\tsin(x)");
-        System.out.println("---------------------");
+        return count;
+    }
+
+    public static int sumOddAboveDiagonal(int[][] matrix) {
+        int sum = 0;
+        int n = matrix.length;
 
         for (int i = 0; i < n; i++) {
-            int x = i * 10;
-            double sinX = sinValues[i];
-            System.out.println(x + "\t\t" + sinX);
+            for (int j = i + 1; j < n; j++) {
+                if (matrix[i][j] % 2 != 0) {
+                    sum += matrix[i][j];
+                }
+            }
         }
+
+        return sum;
     }
 }
