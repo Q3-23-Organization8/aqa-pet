9dd34d0c93d76f7abb7c0e42f6f61fb5b671fb72
POlesya <olesya.pilip@gmail.com>
2023-10-05 22:07:00 +0300

    Task 4

--- Homework_5/main_1.java
+++ Homework_5/main_1.java
@@ -1,43 +1,24 @@
 package Homework_5;
 
-import java.util.Random;
-
 public class main_1 {
     public static void main(String[] args) {
-        int[] array = generateRandomArray(8, 1, 60); // Generate an array of 8 random numbers in the range from 1 to 60
+        int n = 37; // Number of values sin(x)
+        double[] sinValues = new double[n];
 
-        System.out.println("Initial array:");
-        printArray(array);
-
-        replaceEvenNumbersWithZero(array);
-
-        System.out.println("\nArray after replacing even numbers with 0:");
-        printArray(array);
-    }
-
-    public static int[] generateRandomArray(int size, int min, int max) {
-        int[] array = new int[size];
-        Random random = new Random();
-
-        for (int i = 0; i < size; i++) {
-            array[i] = random.nextInt(max - min + 1) + min;
+        for (int i = 0; i < n; i++) {
+            int x = i * 10;
+            double sinX = Math.sin(Math.toRadians(x));
+            sinValues[i] = sinX;
         }
 
-        return array;
-    }
+        // Let's display the table of values sin(x)
+        System.out.println("x\t\tsin(x)");
+        System.out.println("---------------------");
 
-    public static void replaceEvenNumbersWithZero(int[] array) {
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] % 2 == 0) {
-                array[i] = 0;
-            }
+        for (int i = 0; i < n; i++) {
+            int x = i * 10;
+            double sinX = sinValues[i];
+            System.out.println(x + "\t\t" + sinX);
         }
-    }
-
-    public static void printArray(int[] array) {
-        for (int num : array) {
-            System.out.print(num + " ");
-        }
-        System.out.println();
     }
 }
