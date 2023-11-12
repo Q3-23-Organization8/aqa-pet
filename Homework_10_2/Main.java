e2f18cc29dae1b72f583d32042159fc0c1c65d8e
POlesya <olesya.pilip@gmail.com>
2023-11-12 20:18:38 +0200

    Homework_10_2_upd

--- Homework_10_2/Main.java
+++ Homework_10_2/Main.java
@@ -1,28 +1,30 @@
 package Homework_10_2;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> numbers = new java.util.ArrayList<>(List.of(3, 8, 2, 10, 5, 7, 6));
-        int X = 5;
+        List<Integer> numbers = new ArrayList<>(Arrays.asList(3, 8, 2, 10, 5, 7, 6));
+        int x = 5;
 
         int i = 0;
         int j = numbers.size() - 1;
 
         while (i < j) {
-            if (numbers.get(i) > X && numbers.get(j) <= X) {
+            if (numbers.get(i) > x && numbers.get(j) <= x) {
                 // We rearrange the elements if the number from the left end is greater than X, and from the right - no more than X
                 int temp = numbers.get(i);
                 numbers.set(i, numbers.get(j));
                 numbers.set(j, temp);
             }
 
-            if (numbers.get(i) <= X) {
+            if (numbers.get(i) <= x) {
                 i++;
             }
 
-            if (numbers.get(j) > X) {
+            if (numbers.get(j) > x) {
                 j--;
             }
         }
