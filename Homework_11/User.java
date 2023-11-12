5609f10ff61483e33c630d23c72ef00b008ab3d5
POlesya <olesya.pilip@gmail.com>
2023-11-12 21:44:18 +0200

    Homework_11_3

---
+++ Homework_11/User.java
@@ -0,0 +1,49 @@
+package Homework_11;
+
+public class User {
+    private String firstName;
+    private String secondName;
+    private int age;
+
+    public User(String firstName, String secondName, int age) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.age = age;
+    }
+
+    // Getter and setter methods for firstName
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    // Getter and setter methods for secondName
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    // Getter and setter methods for age
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "firstName='" + firstName + '\'' +
+                ", secondName='" + secondName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
