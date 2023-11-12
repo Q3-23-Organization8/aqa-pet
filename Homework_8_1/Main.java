f64324c7caf63f1a549230c217192c94b58564cd
POlesya <olesya.pilip@gmail.com>
2023-11-12 21:43:12 +0200

    Homework_11_1 (upd_homework_8_1)

--- Homework_8_1/Main.java
+++ Homework_8_1/Main.java
@@ -9,18 +9,30 @@ public class Main {
         printableItems[2] = new Book("Dead man’s money", "John Escott");
         printableItems[3] = new Magazine("«Amazonia Investiga»", "EDITORIAL PRIMMATE S.A.S");
 
-        // call the print() method for each object in the array
-        printableItems[0].print();
-        System.out.println();
-        printableItems[1].print();
-        System.out.println();
-        printableItems[2].print();
-        System.out.println();
-        printableItems[3].print();
-        System.out.println();
+        // Using lambda expression to print each item in the array
+        for (Printable printable : printableItems) {
+            printable.print();
+            System.out.println();
+        }
 
-        Magazine.printMagazines(printableItems); //Outputting Magazines only
+        // Using lambda expression to print only magazines
+        printFiltered(printableItems, item -> item instanceof Magazine);
 
-        Book.printBooks(printableItems); //Outputting Books only
+        // Using lambda expression to print only books
+        printFiltered(printableItems, item -> item instanceof Book);
     }
+
+    // Generic method to print items based on a filter
+    private static void printFiltered(Printable[] printableItems, PrintFilter filter) {
+        for (Printable item : printableItems) {
+            if (filter.test(item)) {
+                item.print();
+                System.out.println();
+            }
+        }
+    }
+}
+
+interface PrintFilter {
+    boolean test(Printable printable);
 }
